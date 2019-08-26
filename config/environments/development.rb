0a43e1e99edf15a470a0e5595c1d819ed04c70e0
Aleksandr Bogovid <xander.rei@yandex.ru>
2019-08-26 17:47:06 +0300

    fix image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_controller.asset_host = ENV['ASSET_HOST']
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
